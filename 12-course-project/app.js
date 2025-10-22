08e38819e1a57b5689be4a998c60df866ae56041
jank0 <jan.zelinka12@gmail.com>
2025-10-22 18:25:59 +0200

    special attack

---
+++ 12-course-project/app.js
@@ -0,0 +1,49 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      roundNumber: 0,
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+    isSpecialAttackDisabled() {
+      return this.roundNumber % 3 < 2;
+    },
+  },
+  methods: {
+    attackMonster() {
+      this.roundNumber++;
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+
+      this.specialAttackPlayer();
+    },
+    specialAttackPlayer() {
+      const attackValue = getRandomValue(10, 25);
+      this.playerHealth -= attackValue;
+      this.roundNumber = 0;
+    },
+  },
+});
+
+app.mount("#game");
