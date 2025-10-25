66af8fd9e7968ba6267f24b4bdc293480e08e325
jank0 <jan.zelinka12@gmail.com>
2025-10-25 19:51:52 +0200

    init commit

---
+++ 16-custom-events-props-practice/src/components/ActiveUser.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    userName: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ 16-custom-events-props-practice/src/components/UserData.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <form></form>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    userName: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
