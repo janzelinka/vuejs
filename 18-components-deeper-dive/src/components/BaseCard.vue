82c37828c16dc5a4d14daa771d97881606209434
jank0 <jan.zelinka12@gmail.com>
2025-10-26 17:33:25 +0100

    slots

---
+++ 18-components-deeper-dive/src/components/BaseCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  slots: ["default"],
+};
+</script>
+
+<style scoped>
+section {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+
+section div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
