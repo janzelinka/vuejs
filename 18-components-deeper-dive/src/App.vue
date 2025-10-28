73ae35a44b00759a3ac0f8db70783feef35b439c
jank0 <jan.zelinka12@gmail.com>
2025-10-28 18:59:23 +0100

    scoped slots

--- 18-components-deeper-dive/src/App.vue
+++ 18-components-deeper-dive/src/App.vue
@@ -7,18 +7,26 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+      </template>
+    </course-goals>
   </div>
 </template>
 
 <script>
 import BadgeList from "./components/BadgeList.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 import TheHeader from "./components/TheHeader.vue";
 import UserInfo from "./components/UserInfo.vue";
+
 export default {
   components: {
     TheHeader,
     BadgeList,
     UserInfo,
+    CourseGoals,
   },
   data() {
     return {
---
+++ 18-components-deeper-dive/src/components/CourseGoals.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="(goal, index) in goals" :key="index">
+      <slot :item="goal"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: [
+        "Finish the course and learn Vue!",
+        "Master Vue and build amazing apps",
+      ],
+    };
+  },
+};
+</script>
