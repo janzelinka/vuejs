18bc3c6bfe0cac4999aa9a58d965b59eaa9f20e9
jank0 <jan.zelinka12@gmail.com>
2025-10-28 19:07:13 +0100

    dynamics components prep work

--- 18-components-deeper-dive/src/App.vue
+++ 18-components-deeper-dive/src/App.vue
@@ -1,41 +1,45 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals>
-      <template #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-      </template>
-    </course-goals>
+
+    <button @click="switchComponent('active-goals')">Active Goals</button>
+    <button @click="switchComponent('manage-goals')">Manage Goals</button>
+    <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>
   </div>
 </template>
 
 <script>
-import BadgeList from "./components/BadgeList.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 import TheHeader from "./components/TheHeader.vue";
-import UserInfo from "./components/UserInfo.vue";
+// import UserInfo from "./components/UserInfo.vue";
 
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
   },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    switchComponent(componentName) {
+      this.selectedComponent = componentName;
+    },
   },
 };
 </script>
