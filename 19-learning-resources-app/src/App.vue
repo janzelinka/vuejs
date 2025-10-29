eb94febd329ea6e2c9394c5b040e7bca861ad5b8
jank0 <jan.zelinka12@gmail.com>
2025-10-29 17:34:09 +0100

    initial-commit, first component

---
+++ 19-learning-resources-app/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul>
+    <list-item
+      v-for="resource in resources"
+      :key="resource.id"
+      :resource="resource"
+    />
+  </ul>
+</template>
+
+<script>
+import ListItem from './components/ListItem.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ListItem,
+  },
+  data() {
+    return {
+      resources: [
+        {
+          id: 1,
+          title: 'Vue.js Guide',
+          url: 'https://vuejs.org/guide/',
+          description: 'The official Vue.js guide.',
+        },
+        {
+          id: 2,
+          title: 'Vue Router',
+          url: 'https://router.vuejs.org/',
+          description: 'The official router for Vue.js.',
+        },
+        {
+          id: 3,
+          title: 'Vuex',
+          url: 'https://vuex.vuejs.org/',
+          description:
+            'State management pattern + library for Vue.js applications.',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ 19-learning-resources-app/src/components/ListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <a :href="resource.url" target="_blank" rel="noopener">{{
+      resource.title
+    }}</a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      type: {
+        url: String,
+        title: String,
+      },
+      required: true,
+    },
+  },
+};
+</script>
