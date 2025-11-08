0417accc3364f32f44fa16c09eb6e0f3629cc3c9
jank0 <jan.zelinka12@gmail.com>
2025-11-08 13:13:53 +0100

    programatical navigation with router

--- 22-routing/src/components/users/UsersList.vue
+++ 22-routing/src/components/users/UsersList.vue
@@ -1,6 +1,12 @@
 <template>
+  <button @click="confirmInput()">Go to teams</button>
   <ul>
-    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+    <user-item
+      v-for="user in users"
+      :key="user.id"
+      :name="user.fullName"
+      :role="user.role"
+    ></user-item>
   </ul>
 </template>
 
@@ -12,6 +18,13 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  methods: {
+    confirmInput() {
+      this.$router.push('/teams');
+      // this.$router.back();
+      // this.$router.forward();
+    },
+  },
 };
 </script>
 
