2fa38fe4b02265d36ee1b596adcb375094c437c3
jank0 <jan.zelinka12@gmail.com>
2025-11-08 13:36:48 +0100

    dynamic parameter routing

--- 22-routing/src/main.js
+++ 22-routing/src/main.js
@@ -3,12 +3,17 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
+    // this should be put above dynamic data else it will never match, because
+    // dynamic data matches also /new
+    // { path: '/teams/new', component: TeamsList },
+    { path: '/teams/:teamId', component: TeamMembers },
   ],
 });
 
