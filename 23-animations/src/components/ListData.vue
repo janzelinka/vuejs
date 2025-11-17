7658cfcf45ead1abe4750f0d0319dd59af17fb47
jank0 <jan.zelinka12@gmail.com>
2025-11-17 17:04:50 +0100

    adnimations, prepare

---
+++ 23-animations/src/components/ListData.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul>
+    <li v-for="user in users" :key="user" @click="removeUser(user)">
+      {{ user }}
+    </li>
+  </ul>
+  <div>
+    <input type="text" v-model="user" />
+    <button @click="addUser">Add User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: '',
+      users: ['First', 'Second', 'Other'],
+    };
+  },
+  methods: {
+    addUser() {
+      this.users.push(this.user);
+    },
+    removeUser(user) {
+      this.users = this.users.filter((x) => x != user);
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 1rem 0;
+  padding: 0;
+}
+
+li {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
