65e373d439adfa3e01d3d6213568e3fde240393c
jank0 <jan.zelinka12@gmail.com>
2025-11-17 11:19:08 +0100

    scrollBehavior on router

--- 22-routing/src/main.js
+++ 22-routing/src/main.js
@@ -33,6 +33,14 @@ const router = createRouter({
 
     { path: '/:notFound(.*)', redirect: { path: 'teams' } },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) return savedPosition;
+    else
+      return {
+        top: 0,
+        left: 0,
+      };
+  },
 });
 
 const app = createApp(App);
